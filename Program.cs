e0ae91857a9bf3ab4393991011e5a4c2461bb1af
xingzheng <xingzheng@hotmail.com>
2022-10-23 16:30:35 +0800

    Use WinRT API in .NET 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Windows.Devices.Radios;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+foreach (var radio in await Radio.GetRadiosAsync())
+{
+    Console.WriteLine($"Name:{radio.Name}, Kind:{radio.Kind}, State:{radio.State}");
+}
+
+Console.ReadLine();
